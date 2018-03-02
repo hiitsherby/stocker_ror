611e7322eb3ba147dd48f32da1a301707998e98d
hiitsherby <weiwenyen1123@gmail.com>
2018-03-02 05:53:47 +0000

    crud/ correct_user

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,6 @@
 class StocksController < ApplicationController
   before_action :set_stock, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   # GET /stocks
   # GET /stocks.json
@@ -70,5 +71,8 @@ class StocksController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def stock_params
       params.require(:stock).permit(:ticker, :user_id)
+    end
+    
+    def correct_user 
     end
 end
